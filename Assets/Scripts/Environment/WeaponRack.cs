ffaf05fc6ace62d070bf7a5cc9f19f503b1c63ba
Zilant1337 <97731052+Zilant1337@users.noreply.github.com>
2025-10-07 16:27:43 +0300

    added a textbox for the gun rack

---
+++ Assets/Scripts/Environment/WeaponRack.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WeaponRack : MonoBehaviour
+{
+    [SerializeField]
+    Weapon weapon;
+    [SerializeField]
+    Transform weaponPositionTransform;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
