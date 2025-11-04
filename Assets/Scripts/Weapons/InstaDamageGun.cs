fd9a8d864f11eca39de4084daca255b6ba6fbd09
Zilant1337 <97731052+Zilant1337@users.noreply.github.com>
2025-11-04 21:19:13 +0300

    Added a melee weapon

---
+++ Assets/Scripts/Weapons/InstaDamageGun.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaDamageGun : Gun
+{
+    protected List<Health> healthsToDamage = new List<Health>();
+    private void OnTriggerEnter(Collider other)
+    {
+        Health health = other.GetComponent<Health>();
+        if (health != null && !healthsToDamage.Contains(health))
+        {
+            healthsToDamage.Add(health);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Health health = other.GetComponent<Health>();
+        if (health != null)
+        {
+            healthsToDamage.Remove(health);
+        }
+    }
+    protected virtual void DealDamage(List<Health> healthList)
+    {
+        foreach (Health health in healthList)
+        {
+            if (health != null)
+                health.TakeDamage(damagePerBullet);
+        }
+    }
+    public override void Shoot()
+    {
+        if((ammoCountInMag > 0 || hasInfiniteAmmo) && cooldownTimer == 0 && reloadTimer == 0)
+        {
+            shootAudioSource.PlayOneShot(shootAudioSource.clip);
+            foreach (Health health in healthsToDamage)
+            {
+                if (health != null)
+                    health.TakeDamage(damagePerBullet);
+            }
+            cooldownTimer = timeBetweenShots;
+            // Отнимаем количество патронов в магазине
+            ammoCountInMag--;
+            // Вызываем событие обновления интерфейса
+            if (!hasInfiniteAmmo)
+                AmmoCounter.ammoCountChanged.Invoke(ammoCountInMag, ammoCount);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapons/Projectiles/InstaDamageProjectile.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class InstaDamageProjectile : Projectile
+{
+    protected override void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
