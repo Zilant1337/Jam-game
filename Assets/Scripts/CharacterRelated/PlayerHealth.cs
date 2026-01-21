923b4c637190ec367fccc0fede7b18ddfe90ee31
Zilant1337 <97731052+Zilant1337@users.noreply.github.com>
2026-01-21 22:59:21 +0300

    Adjusted auto shotgun stats

--- Assets/Scripts/CharacterRelated/PlayerHealth.cs
+++ Assets/Scripts/CharacterRelated/PlayerHealth.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public class PlayerHealth:Health
