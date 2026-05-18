3effee5821253c6c0fb2ba47d3ab11924b7d0198
Zilant1337 <grigorenko_p@inbox.ru>
2026-05-18 12:07:18 +0300

    Added a font for note

--- Assets/Scripts/UI/HintNote.cs
+++ Assets/Scripts/UI/HintNote.cs
@@ -1,8 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HintNote : MonoBehaviour
 {
     [SerializeField]
     TMP_Text noteText;
+    [SerializeField]
+    private Transform noteOOSPosition;
+    [SerializeField]
+    private Transform noteOnScreenPosition;
+
+    public static HintNote Instance;
+
+    private void Start()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError($"Can't have more than one HintNote. {Instance.gameObject.name} already exists");
+        }
+    }
+
+    public void ChangeText(string newText)
+    {
+        noteText.text = newText;
+    }
 }
